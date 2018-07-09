d34adce8cef8556f3cbae905441e0ec69627bb9a
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-07-09 14:09:00 +0530

    learnt event handler

--- src/App.js
+++ src/App.js
@@ -11,11 +11,21 @@ class App extends Component {
             { name: 'Stephanie', age: 26 }
         ]
     }
+    switchNameHandler = () => {
+        this.setState({
+            persons: [
+                { name: 'updated_name', age: 28},
+                { name: 'Manu', age: 39},
+                { name: 'Stephanie', age: 30 }
+            ]
+        })
+    }
 
   render() {
     return (
       <div className="App">
         <h1>Welcome to Burger App</h1>
+          <button onClick={this.switchNameHandler}>Change</button>
           <Person name = {this.state.persons[0].name} age= "22" >Hobbies : Gym</Person>
           <Person name = {this.state.persons[1].name} age= "24" />
       </div>
