1b3d4f0c6d089a5d9e8b06988ab39f302282699f
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-07-09 13:44:00 +0530

    learnt use of props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to Burger App</h1>
+          <Person name = "gagan" age= "22" >Hobbies : Gym</Person>
+          <Person name = "baban" age= "24" />
       </div>
     );
   }
